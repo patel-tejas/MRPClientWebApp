ef7ba0f93fcc3341dc748d65ac9729d4c038cb5b
Tejas Patel <pateltejas.2005@gmail.com>
2025-04-19 12:28:56 +0530

    added prod images

--- app/products/fly-ash-brick-pallet/page.tsx
+++ app/products/fly-ash-brick-pallet/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CheckCircle } from "lucide-react"
-import palletmain from "@/public/pallet-main2.png"
+import palletmain from "@/public/pallet-main.png"
 import flyash from "@/public/flyash.jpg"
 
 export default function FlyAshBrickPalletPage() {
