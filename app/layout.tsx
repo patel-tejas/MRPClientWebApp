edf6788096bf435c39f448245990edc91e9322cc
Tejas Patel <pateltejas.2005@gmail.com>
2025-04-23 12:06:55 +0530

    updated metadata for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import FloatingContactButton from "@/components/FloatingContact"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Head from "next/head"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -18,7 +19,34 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: "MRP Exim Products – High-Quality Shuttering Plywood & Pallets",
   description:
-    "MRP Exim Products is a leading manufacturer of premium shuttering plywood and fly ash brick pallets. Discover our high-abrasion metallic film face plywood engineered for superior performance and cost effectiveness.",
+    "MRP Exim Products- leading manufacturer of premium shuttering plywood and fly ash brick pallets. Durable & Cost-effective and industrial needs.",
+  keywords:
+    "MRP Exim Products, shuttering plywood, fly ash brick pallets, high-quality plywood, durable pallets, cost-effective solutions, industrial needs, plywood manufacturer, India",
+  authors: [
+    {
+      name: "MRP Exim Products",
+      url: "https://www.mrpeximproducts.com",
+    },
+  ],
+  creator: "MRP Exim Products",
+  publisher: "MRP Exim Products",
+  openGraph: {
+    title: "MRP Exim Products – High-Quality Shuttering Plywood & Pallets",
+    description:
+      "MRP Exim Products- leading manufacturer of premium shuttering plywood and fly ash brick pallets. Durable & Cost-effective and industrial needs.",
+    url: "https://www.flyashbrickpallet.com",
+    siteName: "MRP Exim Products",
+    images: [
+      {
+        url: "https://www.flyashbrickpallet/logo.png",
+        width: 800,
+        height: 600,
+        alt: "MRP Exim Products Logo",
+      },
+    ],
+    locale: "en-IN",
+    type: "website",
+  }
 };
 export default function RootLayout({
   children,
@@ -27,6 +55,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="next-size-adjust" content="none" />
+      </Head>
       <body className={`${montserrat.variable} font-montserrat`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <TooltipProvider>
